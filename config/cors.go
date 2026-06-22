e355abc01aae403586e721ed2f4c1c5210028ef1
MuhRifa2024 <muhamad.rifaldi174@gmail.com>
2026-06-22 16:00:17 +0700

    add cors

--- config/cors.go
+++ config/cors.go
@@ -4,6 +4,7 @@ var allowedOrigins = []string{
 	"http://localhost:5173",
 	"http://localhost:5174",
 	"https://belatihan-production.up.railway.app",
+	"https://my-fe-rho.vercel.app",
 }
 
 func GetAllowedOrigins() []string {
